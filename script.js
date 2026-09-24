a47731f2315cc2ed4ee9a56017c856f21bac7389
Dimitrios <jimspcs122@gmail.com>
2026-09-24 10:51:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,22 +7,51 @@ const result = document.getElementById("result");
 let yesScale = 1;
 
 
+// Google Sheet URL
+const SHEET_URL =
+    "https://script.google.com/macros/s/AKfycbz1zREmzSbmRfcYNqyDA7DvU6uRxN_um1D_NsP3_GFiIMGNljA-aV4PQ74F4Dq0wJUQ/exec";
+
+
+// Send answer to Google Sheet
+function sendAnswer(answer) {
+
+    fetch(SHEET_URL, {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: "answer=" + encodeURIComponent(answer)
+    });
+
+}
+
+
+// NO BUTTON
 noButton.addEventListener("click", function () {
 
-yesScale += 0.35;
+    // Send NO to Google Sheet
+    sendAnswer("NO");
 
-yesButton.style.transform =
-    `scale(${yesScale})`;
+    // Make YES button bigger
+    yesScale += 0.35;
 
+    yesButton.style.transform =
+        `scale(${yesScale})`;
 
 });
 
 
+// YES BUTTON
 yesButton.addEventListener("click", function () {
 
-question.style.display = "none";
+    // Send YES to Google Sheet
+    sendAnswer("YES");
 
-result.style.display = "block";
+    // Hide question
+    question.style.display = "none";
 
+    // Show result
+    result.style.display = "block";
 
 });
